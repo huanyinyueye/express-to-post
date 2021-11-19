13b6bca69fb9da0ba2144b78f2311f0ddcab6cc8
huanyinyueye <lightfugitive@gmail.com>
2021-11-19 09:58:28 +0800

    Send data

--- myapp.js
+++ myapp.js
@@ -3,17 +3,26 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 var server = http.createServer(app);
-var content = fs.readFileSync('index.html')
+var content = fs.readFileSync('index.html');
+var bodyParser = require('body-parser')
 
-app.use('/css',express.static('css'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/',function(req,res){
-  res.writeHead(200,{'Content-type':'text/html'});
+app.post("/send",(req,res)=>{
+    console.log(req.body);
+    res.send("success")
+})
+
+
+app.use('/css', express.static('css'));
+
+app.get('/', function (req, res) {
+  res.writeHead(200, { 'Content-type': 'text/html' });
   res.write(content);
-  res.end()
+  res.end();
 });
 
-server.listen(8080,'127.0.0.1',function(){
-  console.log("Link Start!")
-})
- 
+server.listen(8080, '127.0.0.1', function () {
+  console.log("Link Start!");
+  console.log("Server at: http://127.0.0.1:8080/");
+});
